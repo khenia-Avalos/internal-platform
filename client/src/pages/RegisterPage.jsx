e0ad0f33f9ababc47150c976e725831f8b81b174
khenia-Avalos <kheniafuentesavalos@gmail.com>
2025-12-07 13:04:29 -0600

    react router

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -3,7 +3,7 @@ import {useForm} from 'react-hook-form'
 import {useAuth} from "../context/authContext"
 import {useEffect} from "react";
 import {useNavigate} from "react-router";
-import {Link} from "react-router-dom"
+import {Link} from "react-router";
 
 function RegisterPage(){
 const {register, handleSubmit, 
