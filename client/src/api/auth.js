58ecea865908d129db28f89085607066d581008c
khenia-Avalos <kheniafuentesavalos@gmail.com>
2025-12-09 20:03:32 -0600

    cambios

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,17 +1,6 @@
-import axios from './axios';
+import axios from "./axios";
 
-export const registerRequest = async (user) => {
-    return axios.post('/auth/register', user);
-};
-
-export const loginRequest = async (user) => {
-    return axios.post('/auth/login', user);
-};
-
-export const verifyTokenRequest = async () => {
-    return axios.get('/auth/verify');
-};
-
-export const logoutRequest = async () => {
-    return axios.get('/auth/logout');
-};
+export const registerRequest = (user) => axios.post("/auth/register", user);
+export const loginRequest = (user) => axios.post("/auth/login", user);
+export const verifyTokenRequest = () => axios.get("/auth/verify");
+export const logoutRequest = () => axios.get("/auth/logout");
