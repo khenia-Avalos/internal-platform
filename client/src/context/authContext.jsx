9a164a36233bd45a70a9afb01625a7944ad1025d
khenia-Avalos <kheniafuentesavalos@gmail.com>
2025-12-13 12:19:20 -0600

    hook and usecontext

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState , useContext} from "react";
 import Cookies from "js-cookie";
 
 import {
@@ -9,6 +9,15 @@ import {
 } from "../api/auth";
 
 export const AuthContext = createContext();
+
+// ✅ ✅ ✅ AÑADE ESTE HOOK - ES LO QUE FALTA ✅ ✅ ✅
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
