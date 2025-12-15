9e018fbed12eb90e0378fe53cb9af4705b497066
khenia-Avalos <kheniafuentesavalos@gmail.com>
2025-12-15 13:47:03 -0600

    usestate.

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,7 @@
 
 import {useForm} from 'react-hook-form'
 import {useAuth} from "../context/authContext"
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useNavigate} from "react-router";
 import {Link} from "react-router";
 
