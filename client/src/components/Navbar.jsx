bde830b67ad2808db10f80f7958551e403f4b1b2
khenia-Avalos <kheniafuentesavalos@gmail.com>
2025-12-21 17:41:04 -0600

    size

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -113,7 +113,7 @@ function Navbar() {
               <Link
                 to="/login"
                 onClick={handleNavClick}
-                className="bg-cyan-600 text-white hover:bg-cyan-700 px-3 py-1.5 rounded-lg font-medium transition block md:inline-block"
+                className="bg-cyan-600 text-white hover:bg-cyan-700 px-1.5 py-2 rounded-lg font-medium transition block md:inline-block"
               >
                 Login
               </Link>
@@ -122,7 +122,7 @@ function Navbar() {
               <Link
                 to="/register"
                 onClick={handleNavClick}
-                className="bg-white text-cyan-600 border border-cyan-600 hover:bg-cyan-50 px-3 py-1.5 rounded-lg font-medium transition block md:inline-block"
+                className="bg-white text-cyan-600 border border-cyan-600 hover:bg-cyan-50 px-1.5 py-2 rounded-lg font-medium transition block md:inline-block"
               >
                 Register
               </Link>
