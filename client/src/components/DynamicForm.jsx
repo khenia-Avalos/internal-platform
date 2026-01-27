a545c59452003a10eb014609acd5f12a807cce71
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-01-27 16:54:50 -0600

    success message

--- client/src/components/DynamicForm.jsx
+++ client/src/components/DynamicForm.jsx
@@ -7,6 +7,7 @@ export const DynamicForm = ({
   fields = [],
   onSubmit,
   errors = [],
+   successMessage = "",
   submitLabel = "Enviar",    
   redirect = {}, 
    isLoading = false
@@ -62,6 +63,18 @@ export const DynamicForm = ({
             {error}
           </div>
         ))}
+           {successMessage && (
+          <div className="mb-4 p-4 bg-green-50 border border-green-200 rounded-lg">
+            <div className="flex items-center">
+              <svg className="w-5 h-5 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20">
+                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+              </svg>
+              <span className="text-green-800 font-semibold">¡Éxito!</span>
+            </div>
+            <p className="text-green-700 mt-2">{successMessage}</p>
+          </div>
+        )}
+        
 
         <Link
           to="/"
