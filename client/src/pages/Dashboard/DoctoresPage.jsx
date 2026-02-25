615582054d75f4f15efeafe9c11bd10a8ef019c9
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-02-24 20:17:31 -0600

    boton

--- client/src/pages/Dashboard/DoctoresPage.jsx
+++ client/src/pages/Dashboard/DoctoresPage.jsx
@@ -1,4 +1,25 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import axios from "axios";
+
 function DoctoresPage(){
+
+const [doctores, setDoctores] = useState([]);  // ← array vacío para la lista
+
+useEffect(() => {
+  const obtenerDoctores = async () => {
+    try {
+      // 1. Hacer petición GET
+      // 2. Guardar respuesta en setDoctores
+    } catch (error) {
+      // 3. Manejar error
+    }
+  };
+  
+  obtenerDoctores(); 
+}, []);
+
     return (
         <div>doctoresPAGE</div>
     )
