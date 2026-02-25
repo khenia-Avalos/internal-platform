c7e991a1f87f051e88756ce3465f169882bcb095
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-02-24 20:22:12 -0600

    boton en page

--- client/src/pages/Dashboard/DoctoresPage.jsx
+++ client/src/pages/Dashboard/DoctoresPage.jsx
@@ -20,8 +20,23 @@ useEffect(() => {
   obtenerDoctores(); 
 }, []);
 
-    return (
-        <div>doctoresPAGE</div>
-    )
+   return (
+  <div className="p-6">
+    <div className="flex justify-between items-center mb-6">
+      <h1 className="text-2xl font-bold">Gestión de Doctores</h1>
+      <button className="bg-cyan-600 text-white px-4 py-2 rounded hover:bg-cyan-700">
+        + Nuevo Doctor
+      </button>
+    </div>
+
+    {doctores.length === 0 ? (
+      <p className="text-gray-500">No hay doctores. Crea el primero.</p>
+    ) : (
+      <table>
+        {/* aquí irá la tabla cuando haya datos */}
+      </table>
+    )}
+  </div>
+);
 }
 export default DoctoresPage
