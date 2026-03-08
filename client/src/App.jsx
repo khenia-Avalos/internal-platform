2783a3d22f5b79cbff27b5771a716a8dcdba41e5
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-03-07 18:14:45 -0600

    import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import ProtectedRoute from "./ProtectedRoute";
 import { TaskProvider } from "./context/TasksContext";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router";
-
+import ClienteDetallePage from "./pages/Dashboard/ClienteDetallePage";
 import ForgotPassword from "./pages/ForgotPassword"; 
 import ResetPassword from "./pages/ResetPassword"; 
 
