c299597f2cd333982bb5cd5263e72d6a929e64cd
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-03-14 13:35:36 -0600

    impor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import { TaskProvider } from "./context/TasksContext";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router";
 import ClienteDetallePage from "./pages/Dashboard/ClienteDetallePage.jsx"; // ← con extensiónimport PacienteDetallePage from "./pages/Dashboard/PacienteDetallePage";
-
+import PacienteDetallePage from "./pages/Dashboard/PacienteDetallePage.jsx"; // ← con extensión
 import ForgotPassword from "./pages/ForgotPassword"; 
 import ResetPassword from "./pages/ResetPassword"; 
 
