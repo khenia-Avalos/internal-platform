ed7a05c4a5c2efd84be1981e8ff5efa149d9edcc
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-03-16 17:46:11 -0600

    agregue todos los dias

--- backend/config/horariosPorDefecto.js
+++ backend/config/horariosPorDefecto.js
@@ -1,5 +1,5 @@
 export const HORARIO_POR_DEFECTO = {
-  dias: [1, 2, 3, 4, 5], // lunes a viernes (0=domingo, 1=lunes...)
+  dias: [1, 2, 3, 4, 5,6,7], // lunes a viernes (0=domingo, 1=lunes...)
   horaInicio: "09:00",
   horaFin: "18:00",
   intervalo: 30,
