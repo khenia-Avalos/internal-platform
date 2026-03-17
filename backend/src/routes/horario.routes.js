35574e1fff8c943523899dba95c69a40cce6b279
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-03-16 18:44:17 -0600

    cambio en routes

--- backend/src/routes/horario.routes.js
+++ backend/src/routes/horario.routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { getHorariosByDoctor, createHorario,updateHorario,deleteHorario } from '../controllers/horario.controller.js';
+import { getHorariosByDoctorRequest, createHorario,updateHorario,deleteHorario } from '../controllers/horario.controller.js';
 import { validateToken } from '../middlewares/validateToken.js';
 
 const router = Router();
 
 // Todas las rutas de horarios requieren autenticación
-router.get('/horarios/doctor/:doctorId', validateToken, getHorariosByDoctor);
+router.get('/horarios/doctor/:doctorId', validateToken, getHorariosByDoctorRequest);
 router.post('/horarios', validateToken, createHorario);
 router.put('/horarios/:id', validateToken, updateHorario);
 router.delete('/horarios/:id', validateToken, deleteHorario);
