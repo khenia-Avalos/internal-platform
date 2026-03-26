54f04f325e4b02ba4464b1d5902b8301700ca982
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-03-26 09:26:19 -0600

    faltaban rutas

--- client/src/api/cita.js
+++ client/src/api/cita.js
@@ -3,3 +3,9 @@ import axios from "./axios";
 
 export const getHorariosDisponiblesRequest = (doctorId, fecha, duracionCita) => 
   axios.get(`/citas/horarios/${doctorId}/${fecha}/${duracionCita}`);
+export const createCita = (cita) => axios.post("/citas", cita);
+export const getCitasByDoctor = () => axios.get("/citas/doctor"); 
+export const getCitasByPaciente = () => axios.get("/citas/paciente");
+export const updateCita = (id, cita) => axios.put(`/citas/${id}`, cita);
+export const deleteCita = (id) => axios.delete(`/citas/${id}`);
+export const getCitaById = (id) => axios.get(`/citas/${id}`);
