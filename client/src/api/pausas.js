cdc7b5d77c6b5bdd74e8dc4ae0938d873da2c15a
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-04-15 13:10:47 -0600

    evitar cachwe con timestamp

--- client/src/api/pausas.js
+++ client/src/api/pausas.js
@@ -2,4 +2,4 @@ import axios from "./axios";
 
 export const iniciarPausaRequest = (data) => axios.post('/pausas/iniciar', data);
 export const terminarPausaRequest = (id) => axios.put(`/pausas/terminar/${id}`);
-export const getPausasActivasRequest = (doctorId) => axios.get(`/pausas/doctor/${doctorId}`);
+export const getPausasActivasRequest = (doctorId) => axios.get(`/pausas/doctor/${doctorId}?t=${Date.now()}`);
