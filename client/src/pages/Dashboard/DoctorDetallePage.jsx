728b16ff7691ecd0878008be3f7647c17a3fc418
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-04-15 13:24:43 -0600

    useefect que recarga al mantener idf url

--- client/src/pages/Dashboard/DoctorDetallePage.jsx
+++ client/src/pages/Dashboard/DoctorDetallePage.jsx
@@ -58,6 +58,25 @@ useEffect(() => {
   };
   if (id) cargarPausaActiva();
 }, [id]);
+// Recargar pausa activa cuando la página recibe foco (al volver de otra pestaña/módulo)
+useEffect(() => {
+  const handleFocus = () => {
+    if (id) {
+      getPausasActivasRequest(id).then(res => {
+        if (res.data.length > 0) {
+          setPausaActiva(res.data[0]);
+        } else {
+          setPausaActiva(null);
+        }
+      });
+    }
+  };
+  
+  window.addEventListener('focus', handleFocus);
+  return () => window.removeEventListener('focus', handleFocus);
+}, [id]);
+
+
 
   const getNombreDia = (dia) => {
   const dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];//los del config por posicion
