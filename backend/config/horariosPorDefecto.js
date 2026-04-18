1b6e90d20303376744a1faa5316ef0e60a8e460d
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-04-18 09:22:00 -0600

    quitar un dia inexistente

--- backend/config/horariosPorDefecto.js
+++ backend/config/horariosPorDefecto.js
@@ -1,5 +1,5 @@
 export const HORARIO_POR_DEFECTO = {
-  dias: [1, 2, 3, 4, 5,6,7], // lunes a viernes (0=domingo, 1=lunes...)
+  dias: [1, 2, 3, 4, 5,6], // lunes a viernes (0=domingo, 1=lunes...)
   horaInicio: "10:00",
   horaFin: "19:00",
   intervalo: 30,
