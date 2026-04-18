facb537bb624f1b3285a625b14a713b8210cff84
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-04-18 09:49:55 -0600

    hprario correcto por defecto

--- backend/config/horariosPorDefecto.js
+++ backend/config/horariosPorDefecto.js
@@ -1,7 +1,7 @@
 export const HORARIO_POR_DEFECTO = {
   dias: [1, 2, 3, 4, 5,6,0], // lunes a viernes (0=domingo, 1=lunes...)
-  horaInicio: "10:00",
-  horaFin: "19:00",
+  horaInicio: "09:00",
+  horaFin: "18:00",
   intervalo: 30,
   activo: true
 };
