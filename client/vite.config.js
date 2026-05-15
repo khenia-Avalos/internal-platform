99c0ed3002a585185972631d7f913b74567c0dbd
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-05-15 12:43:51 -0600

    original , se soluciono refresh con rewite rule en render

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,18 +7,13 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-  base: '/', // ← Asegurar que la base es correcta
-  server: {
-    proxy: {
-      "/api": {
-        target: 'https://el-exito-internal-platform.onrender.com',
-        changeOrigin: true,
-        secure: false,
+  server:{
+    proxy:{
+      "/api":{
+        changeOrigin:true,
+        secure:false,
       }
     }
-  },
-  preview: {
-    port: 3000,
-    strictPort: true,
   }
+  
 })
