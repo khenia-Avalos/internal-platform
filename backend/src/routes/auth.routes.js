13a8b723de3c26618322e747e79dc2ac536eccdd
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-05-27 18:00:01 -0600

    register no utilizado en este sistema

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -6,7 +6,7 @@ import {registerSchema, loginSchema} from '../schemas/auth.schema.js'
 
 const router = Router()
 
-router.post('/register',validateSchema(registerSchema), register);
+// router.post('/register',validateSchema(registerSchema), register);
 router.post('/login', validateSchema(loginSchema), login);
 router.post('/logout', logout);
 router.get('/profile', validateToken, profile)
