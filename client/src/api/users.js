15e4aaecb3074050dab7681f5b64cde0e8388652
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-07-04 12:01:42 -0600

    ruta correcta de endpoint

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const createUserRequest = async (data) => {
-  return await axios.post('/api/auth/register', data);
+  return await axios.post('/api/auth/create-recepcion', data);
 };
