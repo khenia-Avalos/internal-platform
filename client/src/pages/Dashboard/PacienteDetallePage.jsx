2d5979d4e86168d6bbb2f8c396bdf397363f01d5
khenia-Avalos <kheniafuentesavalos@gmail.com>
2026-07-08 17:03:39 -0600

    card

--- client/src/pages/Dashboard/PacienteDetallePage.jsx
+++ client/src/pages/Dashboard/PacienteDetallePage.jsx
@@ -980,7 +980,7 @@ function PacienteDetallePage() {
                         </InfoCard>
                     </div>
 
-                    {/* modal para motivo de fallecimiento */}
+                    {/* modal para motivo de fallecimio */}
                     {mostrarModalFallecimiento && (
                         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
                             <div className="bg-white rounded-xl max-w-md w-full p-6 shadow-2xl">
